957eb00ebc3d03ab2648906547c4de10b84277e8
Игорь <bujhtyz228@gmail.com>
2023-03-21 17:17:14 +0200

    done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,14 +6,18 @@ const ctrl = require("../../controllers/auth");
 
 
 
-router.post("/register", validateBody(schemas.registerSchema), ctrl.register)
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+router.get("/verify/:verificationCode", ctrl.verifyEmail);
+
+router.post("/verify", validateBody(schemas.emailSchema), ctrl.resendVerifyEmail)
 
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
 
-router.post("/logout", authenticate, ctrl.logout)
+router.post("/logout", authenticate, ctrl.logout);
 
-router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
